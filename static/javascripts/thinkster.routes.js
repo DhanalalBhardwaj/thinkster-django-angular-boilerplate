140b4068007c03de0a6f9cb2de24905437370677
Dhanalal Bhardwaj <dhanalalbhardwaj@gmail.com>
2016-04-27 17:30:21 +0530

    routes provider and routes added.

---
+++ static/javascripts/thinkster.routes.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dhanalalb on 27/4/16.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.routes')
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+
+  /**
+  * @name config
+  * @desc Define valid application routes
+  */
+  function config($routeProvider) {
+    $routeProvider.when('/register', {
+      controller: 'RegisterController', 
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).otherwise('/');
+  }
+})();
