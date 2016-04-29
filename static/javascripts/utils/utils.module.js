84817443af08170efa60d818200decaffdf0493e
Dhanalal Bhardwaj <dhanalalbhardwaj@gmail.com>
2016-04-29 15:10:56 +0530

    snackbar service added for snackbarjs

---
+++ static/javascripts/utils/utils.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dhanalalb on 28/4/16.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.utils', [
+      'thinkster.utils.services'
+    ]);
+
+  angular
+    .module('thinkster.utils.services', []);
+})();
