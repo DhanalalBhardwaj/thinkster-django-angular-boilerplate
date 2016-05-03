70f3ce7211623f1765a97a3ea061a6d708c8783d
Dhanalal Bhardwaj <dhanalalbhardwaj@gmail.com>
2016-05-03 17:02:08 +0530

    adde profiles module.

---
+++ static/javascripts/profiles/profiles.module.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dhanalalb on 3/5/16.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster.profiles', [
+            'thinkster.profiles.controllers',
+            'thinkster.profiles.services'
+        ]);
+
+    angular
+        .module('thinkster.profiles.controllers', []);
+
+    angular
+        .module('thinkster.profiles.services', []);
+})();
